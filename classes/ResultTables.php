ca2fd26113bf6ec909e8ab5f705e6975d9d3ec7e
casper <casper@Percivals-MacBook-Pro-2.local>
2017-10-08 20:07:47 +0800

    updated join getResultsTable

--- classes/ResultTables.php
+++ classes/ResultTables.php
@@ -7,8 +7,8 @@ class ResultTables{
     function getResultsTable($db){
         return $db->select("r.resultTableID as id,u.userID userID,concat(ucase(u.lastName),',&nbsp;&nbsp;',u.firstName) as uName,u.userName userName,
         concat(q.questionID,'. ',q.questionText) as question,r.answer as answer")
-        ->from('user u')
-        ->join('resulttable r' ,'r.userID = u.userID')
+        ->from('resulttable r')
+        ->join('user u' ,'u.userID = r.userID')
         ->join('question q','q.questionID = r.questionID')
         ->fetch();
     }
